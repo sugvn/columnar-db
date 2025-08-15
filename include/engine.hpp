983ce3a4c9d58735b3c3f75b1f52cc451196c961
sugvn <suganraj.1211@gmail.com>
2025-08-15 08:58:38 +0530

    refactor(engine):replace 'using namespace std' with specific imports
    change pvt fns return type to Res<T>

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,25 +1,27 @@
 #pragma once
 #include "Result.hpp"
 #include "column.hpp"
-#include "error.hpp"
 #include <fstream>
 #include <string>
+#include <variant>
 #include <vector>
-using namespace std;
+using std::string;
+using std::vector;
+using None=std::monostate;
 
 class Engine {
 
 private:
   bool tableExists(const string &name);
   Res<fstream> openMetaFile(const string &name);
-  void writeMeta(const string &name, const vector<column> &columns);
-  void createColumnFiles(const string &name, const vector<column> &columns,Err *e=nullptr);
-  void createMetaFile(const string &name,Err *e=nullptr);
-  void openColumnFiles(const string &tableName, vector<fstream> &files,const vector<column> &cols,Err *e=nullptr);
+  Res<None> writeMeta(fstream &file, const vector<Column> &columns);
+  Res<None> createColumnFiles(const string &name, const vector<Column> &columns);
+  Res<None> createMetaFile(const string &name);
+  Res<vector<fstream>> openColumnFiles(const string &tableName,const vector<Column> &cols);
 
 public:
-  bool createTable(const string &name, const vector<column> &columns,const column &primaryKey);
-  bool loadColumns(const string &name, vector<column> &columns);
+  bool createTable(const string &name, const vector<Column> &columns,const Column &primaryKey);
+  bool loadColumns(const string &name, vector<Column> &columns);
   bool insertIntoTable(const string &name, const vector<string> &vec);
 
 };
