540ef827e0a5ae1e6adbdd16931ad31e2f616007
sugvn <suganraj.1211@gmail.com>
2025-08-15 15:23:10 +0530

    format(engine):sort private functions
    added comments for future references

--- include/engine.hpp
+++ include/engine.hpp
@@ -7,21 +7,25 @@
 #include <vector>
 using std::string;
 using std::vector;
-using None=std::monostate;
+using None = std::monostate;
 
 class Engine {
 
 private:
+  // general helper
   bool tableExists(const string &name);
+
+  // MetaFile utilities
+  Res<None> createMetaFile(const string &name);
   Res<fstream> openMetaFile(const string &name);
   Res<None> writeMeta(fstream &file, const vector<Column> &columns);
-  Res<None> createColumnFiles(const string &name, const vector<Column> &columns);
-  Res<None> createMetaFile(const string &name);
+
+  // Column(.col) files utilities
+  Res<None> createColumnFiles(const string &name,const vector<Column> &columns);
   Res<vector<fstream>> openColumnFiles(const string &tableName,const vector<Column> &cols);
 
 public:
   bool createTable(const string &name, const vector<Column> &columns,const Column &primaryKey);
   bool loadColumns(const string &name, vector<Column> &columns);
   bool insertIntoTable(const string &name, const vector<string> &vec);
-
 };
