22e2f522bf3c12342bbda992004e69be0bcc3137
sugvn <suganraj.1211@gmail.com>
2025-08-17 12:36:27 +0530

    feat(.clang-format): add better formatting rules
    created .clang-format to define formatting rules
    
    added 8 space indentation while formatting

--- include/engine.hpp
+++ include/engine.hpp
@@ -11,23 +11,26 @@ using None = std::monostate;
 
 class Engine {
 
-private:
-  // general helper
-  bool tableExists(const string &name); //done
+      private:
+        // general helper
+        bool tableExists(const string &name); // done
 
-  // MetaFile utilities
-  Res<None> createMetaFile(const string &name); //done
-  Res<fstream> openMetaFile(const string &name); //done
-  Res<None> writeMeta(const string &name, const vector<Column> &columns);
+        // MetaFile utilities
+        Res<None> createMetaFile(const string &name);  // done
+        Res<fstream> openMetaFile(const string &name); // done
+        Res<None> writeMeta(const string &name, const vector<Column> &columns);
 
-  // Column(.col) files utilities
-  Res<None> createColumnFiles(const string &name,const vector<Column> &columns);
-  Res<vector<fstream>> openColumnFiles(const string &tableName,const vector<Column> &cols);
+        // Column(.col) files utilities
+        Res<None> createColumnFiles(const string &name,
+                                    const vector<Column> &columns);
+        Res<vector<fstream>> openColumnFiles(const string &tableName,
+                                             const vector<Column> &cols);
 
-  //idk what to do with this
-  bool loadColumns(const string &name, vector<Column> &columns);
+        // idk what to do with this
+        bool loadColumns(const string &name, vector<Column> &columns);
 
-public:
-  bool createTable(const string &name, const vector<Column> &columns,const Column &primaryKey);
-  bool insertIntoTable(const string &name, const vector<string> &vec);
+      public:
+        bool createTable(const string &name, const vector<Column> &columns,
+                         const Column &primaryKey);
+        bool insertIntoTable(const string &name, const vector<string> &vec);
 };
