2550e24f98c255bf81723a06b939475a0ed5e977
sugvn <suganraj.1211@gmail.com>
2025-08-20 20:43:37 +0530

    remove db/,include/(engine.hpp,column.hpp)

--- include/Result.hpp
+++ include/Result.hpp
@@ -1,19 +1,17 @@
 #pragma once
 #include "string"
-using namespace std;
 
-struct Err{
+struct Err {
         bool status;
-        string msg;
-        Err():status(false),msg(){};
-        Err(bool s,const string &m):status(s),msg(m){}
+        std::string msg;
+        Err() : status(false), msg() {};
+        Err(bool s, const std::string &m) : status(s), msg(m) {}
         explicit operator bool() const { return status; }
 };
 
-template<typename T>
-struct Res{
+template <typename T> struct Res {
         T value;
         Err e;
-        Res(T v):value(v),e(){}
-        Res(T v,const string &s):value(v),e(true,s){}
+        Res(T v) : value(v), e() {}
+        Res(T v, const std::string &s) : value(v), e(true, s) {}
 };
