1d55a051871b24b05d1b8caae990049232fdb9cc
sugvn <suganraj.1211@gmail.com>
2025-09-09 17:06:53 +0530

    refactor(engine.hpp):change return type of function declaration

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,18 +1,18 @@
-
 #pragma once
 
-//Includes
-#include <string>
-#include <vector>
+// Includes
 #include "column.hpp"
 #include <expected>
+#include <string>
+#include <vector>
+using namespace std;
 
-//Custom return types for easier error handling
-template <typename T>
-using Res=std::expected<T,std::string>;
-using Err=std::unexpected<std::string>;
+// Custom return types for easier error handling
+template <typename T> using Res = std::expected<T, std::string>;
+using Err = std::unexpected<std::string>;
 
 class StorageEngine {
-      public:
-        Res<void> createTable(std::string name, const std::vector<Column> &columns);
+	public:
+	Res<void> createTable(
+		std::string name, const std::vector<Column> &columns);
 };
